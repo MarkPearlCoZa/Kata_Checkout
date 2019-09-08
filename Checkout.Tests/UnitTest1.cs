feec930856bd3d8f8208a7d50465e8159df46d55
Mark Pearl <markpearl@gmail.com>
2019-09-08 20:14:27 +1200

    Scan method started

--- Checkout.Tests/UnitTest1.cs
+++ Checkout.Tests/UnitTest1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
 using NUnit.Framework;
 
 namespace Tests
@@ -10,9 +14,17 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void TestScan()
         {
-            Assert.Pass();
+            var input = new List<char>() {'A'};
+
+            var current = input.Last();
+            var actual = new Dictionary<char, int>();
+            actual.Add(current,1);
+            var result = actual;
+            
+            Assert.That(result, Is.EquivalentTo(new Dictionary<char, int>{{'A',1}}));
+
         }
     }
 }
